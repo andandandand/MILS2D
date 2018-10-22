8d994ae9c0005fe1483700374463195b0a973026
andandandand <antonio.rueda.toicen@gmail.com>
2018-10-22 03:12:09 -0700

    reactive data tests

--- server.R
+++ server.R
@@ -5,8 +5,38 @@ source("scripts/compressionLength.R")
 source("scripts/loadGraph.R")
 source("scripts/edgeAndVertexKnockout.R")
 source("scripts/listEdges.R")
-source("scripts/matrixPlot.R")
+#source("scripts/matrixPlot.R")
 
+
+require("igraph")
+require("ggplot2")
+require("reshape2")
+
+plotAdjMatrix <- function(graphToPlot){
+  
+  gAdjMatrix <- as.matrix(as_adj(graphToPlot))
+  
+  logMatrix <- (gAdjMatrix == 1)
+  
+  matData <- melt(logMatrix)
+  
+  g <- ggplot(data = matData,
+              aes(Var2, Var1)) + 
+    geom_tile(aes(fill = value, 
+                  color = value)) + 
+    coord_equal() + 
+    scale_fill_manual(values = c("TRUE" = "black", "FALSE" = "white")) + 
+    scale_color_manual(values = c("TRUE" = "white", "FALSE" = "black")) + 
+    theme_bw() +
+    theme(axis.title = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid = element_blank()) +
+    guides(fill = FALSE, color = FALSE) +
+    scale_y_reverse()
+  
+  print(g)
+}
 
 shinyServer(function(input, output, session) {
   
@@ -114,7 +144,7 @@ shinyServer(function(input, output, session) {
       #plotAdjMatrix(reactiveData$g) # why does this fail?
       #plotAdjMatrix(make_star(5)) # why does this work? 
       #cheat <- reactiveData$g # fails
-      plotAdjMatrix(cheat)
+      plotAdjMatrix(make_star(8))
       print("test")
     
     }
