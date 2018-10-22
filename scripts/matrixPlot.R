c5365aa636a55fb81bbbff2a694d39c30e22e98b
andandandand <antonio.rueda.toicen@gmail.com>
2018-10-22 01:44:42 -0700

    graphPlot.R

--- scripts/matrixPlot.R
+++ scripts/matrixPlot.R
@@ -4,28 +4,28 @@ require(reshape2)
 
 matrixPlot <- function(graphToPlot){
   
-  gAdjMatrix <- as.matrix(as_adj(g))
+  gAdjMatrix <- as.matrix(as_adj(graphToPlot))
   
-  mm <- (gAdjMatrix == 1)
+  logMatrix <- (gAdjMatrix == 1)
   
-  mm %>% 
-    melt() %>% 
-    ggplot(aes(Var2, Var1)) + 
+  matData <- melt(logMatrix)
+  
+  g <- ggplot(data = matData,
+              aes(Var2, Var1)) + 
     geom_tile(aes(fill = value, 
                   color = value)) + 
     coord_equal() + 
-    scale_fill_manual(values = c("black", "white")) + 
-    scale_color_manual(values = c("white", "black")) + 
+    scale_fill_manual(values = c("TRUE" = "black", "FALSE" = "white")) + 
+    scale_color_manual(values = c("TRUE" = "white", "FALSE" = "black")) + 
     theme_bw() +
     theme(axis.title = element_blank(),
           axis.text = element_blank(),
           axis.ticks = element_blank(),
-          panel.grid = element_blank()) + 
-    guides(fill = FALSE, color = FALSE) + 
-    scale_x_discrete(expand = c(0,0)) + 
-    scale_y_discrete(expand = c(0,0))
+          panel.grid = element_blank()) +
+    guides(fill = FALSE, color = FALSE) +
+    scale_y_reverse()
   
-    print(mm)
+  print(g)
 }
 
 #test
